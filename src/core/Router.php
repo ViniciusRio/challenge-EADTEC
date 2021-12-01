06093fdf63005ea555166d1869324c541b6a5756
ViniciusRio <viniciustrave@gmail.com>
2021-12-01 17:49:36 -0300

    upload(initial handler for routes)

---
+++ src/core/Router.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace src\core;
+
+use Exception;
+
+class Router
+{
+    public array $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
+
+    public static function load($file)
+    {
+        $router = new static();
+
+        require $file;
+
+        return $router;
+    }
+
+    /**
+     * @param string $uri
+     * @param array $action
+     * @return void
+     */
+    public function get(string $uri, string $action): void
+    {
+        $this->routes['GET'][$uri] = $action;
+    }
+
+    public function post(string $uri, string $action): void
+    {
+        $this->routes['POST'][$uri] = $action;
+    }
+
+    public function direct($uri, $requestType)
+    {
+        if (array_key_exists($uri, $this->routes[$requestType])) {
+            return $this->callAction(
+                ...explode('@', $this->routes[$requestType][$uri])
+            );
+        }
+
+        throw new Exception('No route found');
+    }
+
+    public function callAction($controller, $action)
+    {
+        // $params = Request::params();
+        $controller = "\\src\\controllers\\{$controller}";
+        $controller = new $controller();
+
+        // if (!method_exists($controller, $action)) {
+        //     throw new Exception(
+        //         "{$controller} does not respond to the {$action} action."
+        //     );
+        // }
+        // if (empty($params)) {
+        //     return $controller->$action();
+        // }
+
+        return $controller->$action();
+    }
+}
