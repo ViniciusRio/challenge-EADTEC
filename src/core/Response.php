ee95f9182a12626f8156c00af8a87ff543a0acd5
ViniciusRio <viniciustrave@gmail.com>
2021-12-07 15:12:33 -0300

    update(add default headers response)

--- src/core/Response.php
+++ src/core/Response.php
@@ -6,7 +6,12 @@ class Response
 {
     public static function sendJson($params) 
     {
-        header('Content-Type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,PATCH,DELETE");
+        header("Access-Control-Max-Age: 3600");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
         http_response_code(200);
 
         if ($params === 1) {
